74891f519ea1233ff798648d7b4d27b4dc6f98c3
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-09 09:55:53 +0000

    Added first test for deposit and coded to pass

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,11 @@
 class Account
+  attr_reader :balance
 
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(amount)
+    @balance += amount 
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,3 +1,12 @@
 describe Account do
-  
+  let(:account) { described_class.new }
+
+  it { is_expected.to respond_to :balance }
+
+  context 'Deposit money' do
+    it 'the balance updates when you deposit money into the account' do
+      account.deposit(100)
+      expect(account.balance).to eq(100)
+    end
+  end
 end
